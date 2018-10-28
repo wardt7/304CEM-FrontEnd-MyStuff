b6d8c287a98fe2ec887ce997152c355d034b34e1
Toby Ward <wardt7@uni.coventry.ac.uk>
2018-10-28 14:12:48 +0000

    Created a product card component that is clickable. Also removed ESlint from dependencies as it turns out that is already built in. TODO: Dynamically add cards using a JSON file testbed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Search from './components/search/Search'
+import ProductCard from './components/productCard/ProductCard'
 import reactLogo from './logo.svg'
+import testImage from './test1.jpg'
 // eslint-enable no-unusued-vars
 
 class App extends Component {
     onSearch (term) {
 	console.log('search on term:' + term)
+    }
+    onProductClick (id) {
+	console.log('showing full product with id:' + id)
     }
     render () {
 	return (
@@ -18,7 +23,8 @@ class App extends Component {
 		<a href="#default" className="logo">{this.props.title}</a>
                 </div>
                 <div id="Search">
-                <Search></Search>
+                <Search onSearchClick={this.onSearch} />
+		<ProductCard productImg={testImage} productID="1" productName="Big Tobz" productPrice="69.00" productLocation="Coventry, UK" onProductClick={this.onProductClick} /> 
                 </div>
                 </div>
     )
---
+++ my-app/src/components/productCard/ProductCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import './ProductCard.css'
+
+class ProductCard extends Component {
+    constructor (props) {
+	super(props)
+	this.showProduct = this.showProduct.bind(this)
+    }
+    showProduct(event){
+	this.props.onProductClick(this.props.productID)
+    }
+    render () {
+	return(
+		<div className="productCard" onClick={this.showProduct}>
+	        <img src={this.props.productImg} alt="No Image Available :(" />
+	        <p className="productName">{this.props.productName}</p>
+	        <p className="productPrice">£{this.props.productPrice}</p>
+	        <p className="productLocation">{this.props.productLocation}</p>
+		</div>
+	)
+    }
+}
+
+export default ProductCard
