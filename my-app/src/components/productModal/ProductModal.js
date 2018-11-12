05be254ee0f27637b7727cd69175247d85fde4eb
Toby Ward <wardt7@uni.coventry.ac.uk>
2018-11-12 19:58:04 +0000

    Added additional fields to the modal

--- my-app/src/components/productModal/ProductModal.js
+++ my-app/src/components/productModal/ProductModal.js
@@ -10,8 +10,10 @@ class ProductModal extends Component {
 		<div className="productModal">
 		<p className="productModalName">{this.props.product.title}</p>
 		<img src={this.props.product.links[0].href} alt="No Image Avalable :(" />
+		<p className="productModalDescription">Description: {this.props.product.description}</p>
 		<p className="productModalPrice">Price: £{this.props.product.price}</p>
 		<p className="productModalLocation">Location: {this.props.product.location}</p>
+		<p className="productModalAuthor">Seller: {this.props.product.author}</p>
 		</div>
 	)
     }
